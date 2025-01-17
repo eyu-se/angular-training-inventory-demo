677e8110da3f4db598ffb1fec902691effb80df3
eyu-se <eyuelmulatu@gmail.com>
2025-01-17 18:48:03 +0300

    draft

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddWarehouseComponent } from './components/warehouse/add-warehouse/add-warehouse.component';
+import { EditWarehouseComponent } from './components/warehouse/edit-warehouse/edit-warehouse.component';
+import { ListWarehouseComponent } from './components/warehouse/list-warehouse/list-warehouse.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: 'warehouse/add', component: AddWarehouseComponent },
+  { path: 'warehouse/edit/:id', component: EditWarehouseComponent },
+  { path: 'warehouse/list', component: ListWarehouseComponent },
+
+  { path: '', redirectTo: '/warehouse/list', pathMatch: 'full' } 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
